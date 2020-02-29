b8c5d68cb95d5bace2d564642525c85708b51135
TestCommit <maya.maglakelidze@gmail.com>
2020-02-29 12:40:57 -0800

    added new sample class

---
+++ src/day01/AnotherSampleClass.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class AnotherSampleClass {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Capture i = " + i);
+        }
+    }
+}
